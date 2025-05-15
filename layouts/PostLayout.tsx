ae6558b4074a0e13b68c9771401cb4e5dcebf0bc
ferrandisme <51866199+ferrandisme@users.noreply.github.com>
2025-05-15 23:34:15 +0200

    improve post layout

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -55,68 +55,57 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
               </div>
             </div>
           </header>
-          <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0 dark:divide-gray-700">
-            <dl className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
-              <dt className="sr-only">Authors</dt>
-              <dd>
-                <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:block xl:space-y-8 xl:space-x-0">
-                  {authorDetails.map((author) => (
-                    <li className="flex items-center space-x-2" key={author.name}>
-                      {author.avatar && (
-                        <Image
-                          src={author.avatar}
-                          width={38}
-                          height={38}
-                          alt="avatar"
-                          className="h-10 w-10 rounded-full"
-                        />
-                      )}
-                      <dl className="text-sm leading-5 font-medium whitespace-nowrap">
-                        <dt className="sr-only">Nombre</dt>
-                        <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
-                        <dd>
-                          {author.twitter && (
-                            <Link
-                              href={author.twitter}
-                              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                            >
-                              {author.twitter
-                                .replace('https://twitter.com/', '@')
-                                .replace('https://x.com/', '@')}
-                            </Link>
-                          )}
-                        </dd>
-                      </dl>
-                    </li>
-                  ))}
-                </ul>
-              </dd>
-            </dl>
-            <div className="divide-y divide-gray-200 xl:col-span-3 xl:row-span-2 xl:pb-0 dark:divide-gray-700">
-              <div className="prose dark:prose-invert max-w-none pt-10 pb-8">{children}</div>
-              <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
-                {/* TODO: Add functionality or update the link for discussing on Twitter */}
-                {/* <Link href={discussUrl(path)} rel="nofollow">
-                  Comentar en Twitter
-                </Link> 
-                                {` • `}
-*/}
-                <Link href={editUrl(filePath)}>Ver en Github</Link>
+          <div className="divide-y divide-gray-200 pb-8 dark:divide-gray-700">
+            <div className="prose dark:prose-invert max-w-none pt-10 pb-8">{children}</div>
+            <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
+              <Link href={editUrl(filePath)}>Ver en Github</Link>
+            </div>
+            {siteMetadata.comments && (
+              <div className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300" id="comment">
+                <Comments slug={slug} />
               </div>
-              {siteMetadata.comments && (
-                <div
-                  className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300"
-                  id="comment"
-                >
-                  <Comments slug={slug} />
-                </div>
-              )}
+            )}
+            <div className="py-6">
+              <h2 className="mb-4 text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
+                Autores
+              </h2>
+              <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12">
+                {authorDetails.map((author) => (
+                  <li className="flex items-center space-x-2" key={author.name}>
+                    {author.avatar && (
+                      <Image
+                        src={author.avatar}
+                        width={38}
+                        height={38}
+                        alt="avatar"
+                        className="h-10 w-10 rounded-full"
+                      />
+                    )}
+                    <dl className="text-sm leading-5 font-medium whitespace-nowrap">
+                      <dt className="sr-only">Nombre</dt>
+                      <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
+                      <dt className="sr-only">Twitter</dt>
+                      <dd>
+                        {author.twitter && (
+                          <Link
+                            href={author.twitter}
+                            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          >
+                            {author.twitter
+                              .replace('https://twitter.com/', '@')
+                              .replace('https://x.com/', '@')}
+                          </Link>
+                        )}
+                      </dd>
+                    </dl>
+                  </li>
+                ))}
+              </ul>
             </div>
             <footer>
-              <div className="divide-gray-200 text-sm leading-5 font-medium xl:col-start-1 xl:row-start-2 xl:divide-y dark:divide-gray-700">
+              <div className="text-sm leading-5 font-medium">
                 {tags && (
-                  <div className="py-4 xl:py-8">
+                  <div className="py-4">
                     <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
                       Etiquetas
                     </h2>
@@ -128,7 +117,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                   </div>
                 )}
                 {(next || prev) && (
-                  <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
+                  <div className="flex justify-between py-4">
                     {prev && prev.path && (
                       <div>
                         <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
@@ -151,15 +140,15 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                     )}
                   </div>
                 )}
-              </div>
-              <div className="pt-4 xl:pt-8">
-                <Link
-                  href={`/${basePath}`}
-                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                  aria-label="Volver al blog"
-                >
-                  &larr; Volver al blog
-                </Link>
+                <div className="pt-4">
+                  <Link
+                    href={`/${basePath}`}
+                    className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                    aria-label="Volver al blog"
+                  >
+                    &larr; Volver al blog
+                  </Link>
+                </div>
               </div>
             </footer>
           </div>
